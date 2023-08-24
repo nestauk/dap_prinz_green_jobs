e4973d37c0555e523a96811af2bfbd4e6389b3cb
India Kerle <india.kerle@nesta.org.uk>
2023-08-24 10:41:15 +0100

    modify training data to pass different random seed + name change

--- dap_prinz_green_jobs/pipeline/green_measures/industries/prodigy/make_training_data.py
+++ dap_prinz_green_jobs/pipeline/green_measures/industries/prodigy/make_training_data.py
@@ -1,15 +1,18 @@
+import sys
+
+sys.path.append("/Users/india.kerlenesta/Projects/dap_green_jobs/dap_prinz_green_jobs")
 """
 This script generates training data of train_size size.
 
 For example,
 
-    python -m dap_prinz_green_jobs.pipeline.span_extraction.make_training_data --train_size 5000
+    python -m dap_prinz_green_jobs.pipeline.green_measures.industries.prodigy.make_training_data --train_size 1000 --random_seed 64
 """
 from dap_prinz_green_jobs import BUCKET_NAME, PROJECT_DIR, logger
 from dap_prinz_green_jobs.getters.ojo_getters import get_mixed_ojo_sample
-
 import dap_prinz_green_jobs.utils.text_cleaning as tc
 
+from datetime import datetime
 from argparse import ArgumentParser
 import os
 import json
@@ -19,15 +22,17 @@ import boto3
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--train_size", type=int, default=5000)
+    parser.add_argument("--random_seed", type=int, default=42)
 
     args = parser.parse_args()
 
     train_size = args.train_size
+    random_seed = args.random_seed
 
     mixed_ojo = (
         get_mixed_ojo_sample()
         .drop_duplicates(subset="description")
-        .sample(frac=1, random_state=42)[:train_size]  # shuffle
+        .sample(frac=1, random_state=random_seed)[:train_size]  # shuffle
         .reset_index(drop=True)
     )
 
@@ -50,10 +55,13 @@ if __name__ == "__main__":
         converted_training_data += "\n"
 
     logger.info("saving training data to s3...")
-    output_dir = "outputs/data/ojo_application/deduplicated_sample/"
+    output_dir = "inputs/data/training_data/"
+    date = datetime.now().strftime("%Y-%m-%d").replace("-", "")
     s3 = boto3.client("s3")
     s3.put_object(
         Body=converted_training_data,
         Bucket=BUCKET_NAME,
-        Key=os.path.join(output_dir, f"mixed_ojo_sample_{str(train_size)}.jsonl"),
+        Key=os.path.join(
+            output_dir, f"{date}_mixed_ojo_sample_{str(train_size)}.jsonl"
+        ),
     )
