46b662b13cc2bbc949c69148579b931a1116fa47
lizgzil <lizgzil@hotmail.com>
2023-10-12 16:41:42 +0100

    Add information about the occ evaluation

--- dap_prinz_green_jobs/pipeline/green_measures/occupations/evaluation/socmapper_evaluation_sample.py
+++ dap_prinz_green_jobs/pipeline/green_measures/occupations/evaluation/socmapper_evaluation_sample.py
@@ -4,6 +4,7 @@ Create a sample dataset to evaluate how well occupations were linked to SOC
 
 import pandas as pd
 import pyarrow.parquet as pq
+import random
 
 from tqdm import tqdm
 
@@ -15,48 +16,16 @@ from dap_prinz_green_jobs.pipeline.green_measures.occupations.occupations_data_p
     process_job_title_soc,
 )
 
-if __name__ == "__main__":
-    # Get the most common job titles in OJO
 
-    adverts_ojd_daps_extract = pd.read_parquet(
-        config["ojo_s3_file_adverts_ojd_daps_extract"]
-    )
-
-    count_job_titles = adverts_ojd_daps_extract["job_title_raw"].value_counts()
-    print(
-        f"There are a total of {len(count_job_titles)} unique job titles in all of OJO"
-    )
-    common_job_titles = count_job_titles[0:1000].to_dict()
-
-    # Get the SOC name
-
-    jobtitle_soc_data = process_job_title_soc(load_job_title_soc())
-    soc_2020_ext_dict = dict(
-        zip(
-            jobtitle_soc_data["SOC_2020_EXT"],
-            jobtitle_soc_data["SUB-UNIT GROUP DESCRIPTIONS"],
-        )
-    )
-    soc_2020_dict = dict(
-        zip(
-            [int(s) for s in jobtitle_soc_data["SOC_2020"]],
-            jobtitle_soc_data["SOC 2020 UNIT GROUP DESCRIPTIONS"],
-        )
-    )
-
-    # Use SOCmapper
-
-    soc_mapper = SOCMapper()
-    soc_mapper.load()
-    socs = soc_mapper.get_soc(job_titles=list(common_job_titles.keys()))
+def get_evaluation_df(soc_mapper, job_titles_list, soc_2020_ext_dict, soc_2020_dict):
     socs_additional_info = soc_mapper.get_soc(
-        job_titles=list(common_job_titles.keys()), additional_info=True
+        job_titles=list(job_titles_list.keys()), additional_info=True
     )
 
     # Output
     evaluation_data = []
     for (ojo_job_title, num_job_ads), soc_info in zip(
-        common_job_titles.items(), socs_additional_info
+        job_titles_list.items(), socs_additional_info
     ):
         if soc_info["most_likely_soc"]:
             if type(soc_info["most_likely_soc"][0]) == str:
@@ -98,6 +67,57 @@ if __name__ == "__main__":
 
     evaluation_data_df = pd.DataFrame(evaluation_data)
 
-    evaluation_data_df.to_csv(
-        "dap_prinz_green_jobs/pipeline/evaluation/soc_evaluation_sample.csv"
+    return evaluation_data_df
+
+
+if __name__ == "__main__":
+    # Get the most common job titles in OJO
+
+    adverts_ojd_daps_extract = pd.read_parquet(
+        config["ojo_s3_file_adverts_ojd_daps_extract"]
+    )
+
+    count_job_titles = adverts_ojd_daps_extract["job_title_raw"].value_counts()
+    print(
+        f"There are a total of {len(count_job_titles)} unique job titles in all of OJO"
+    )
+    common_job_titles = count_job_titles[0:1000].to_dict()
+
+    random_job_titles = random.sample(list(count_job_titles.items()), 200)
+    random_job_titles = {k: v for (k, v) in random_job_titles}
+
+    # Get the SOC name
+
+    jobtitle_soc_data = process_job_title_soc(load_job_title_soc())
+    soc_2020_ext_dict = dict(
+        zip(
+            jobtitle_soc_data["SOC_2020_EXT"],
+            jobtitle_soc_data["SUB-UNIT GROUP DESCRIPTIONS"],
+        )
+    )
+    soc_2020_dict = dict(
+        zip(
+            [int(s) for s in jobtitle_soc_data["SOC_2020"]],
+            jobtitle_soc_data["SOC 2020 UNIT GROUP DESCRIPTIONS"],
+        )
+    )
+
+    # Use SOCmapper
+
+    soc_mapper = SOCMapper()
+    soc_mapper.load()
+
+    common_evaluation_data_df = get_evaluation_df(
+        soc_mapper, common_job_titles, soc_2020_ext_dict, soc_2020_dict
+    )
+
+    common_evaluation_data_df.to_csv(
+        "dap_prinz_green_jobs/pipeline/green_measures/occupations/evaluation/soc_evaluation_sample.csv"
+    )
+    random_evaluation_data_df = get_evaluation_df(
+        soc_mapper, random_job_titles, soc_2020_ext_dict, soc_2020_dict
+    )
+
+    random_evaluation_data_df.to_csv(
+        "dap_prinz_green_jobs/pipeline/green_measures/occupations/evaluation/soc_evaluation_random_sample.csv"
     )
