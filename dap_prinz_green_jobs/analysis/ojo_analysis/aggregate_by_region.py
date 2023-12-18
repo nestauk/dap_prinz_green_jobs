c8c858641c1e913b4633f09909f062774c849e6d
Liz G <elizabeth.gallagher@nesta.org.uk>
2023-12-18 10:21:03 +0000

    Update chloropleths with new datasets (#114)
    * Update chloropleths with new datasets
    
    * Use itl quotient, just output SIC (not merged to SIC2)
    
    * set analysis config to large sample
    
    * Update chloropleth notebooks
    
    * debug regional comparisons
    
    * fix bug
    
    ---------
    
    Co-authored-by: India Kerle <india.kerle@nesta.org.uk>

--- dap_prinz_green_jobs/analysis/ojo_analysis/aggregate_by_region.py
+++ dap_prinz_green_jobs/analysis/ojo_analysis/aggregate_by_region.py
@@ -20,7 +20,6 @@ import yaml
 import os
 
 if __name__ == "__main__":
-
     job_id_col = analysis_config["job_id_col"]
     data_type = analysis_config["data_type"]
     skill_match_thresh = analysis_config["skill_match_thresh"]
@@ -41,7 +40,7 @@ if __name__ == "__main__":
         salary_information = get_mixed_ojo_salaries_sample()
         locations_information = get_mixed_ojo_location_sample()
     elif data_type == "large":
-        salary_information = get_large_ojo_location_sample()
+        salary_information = get_large_ojo_salaries_sample()
         locations_information = get_large_ojo_location_sample()
     else:
         print("set data_type in config to mixed or large")
@@ -58,61 +57,60 @@ if __name__ == "__main__":
         skill_measures_df, skill_match_thresh=skill_match_thresh
     )
 
-    agg_itl_by = "itl_3_code"  # itl_2_code or itl_3_code
-
-    itl_aggregated_data = pg.create_agg_data(
-        all_green_measures_df,
-        all_skills_df,
-        soc_descriptions_dict=None,
-        agg_col=agg_itl_by,
-        job_id_col="job_id",
-    )
-
-    # Clean up for tooltips in plotting
-    itl_aggregated_data["top_3_sics_names"] = itl_aggregated_data["top_5_sics"].apply(
-        lambda x: ", ".join([s["sic_name"] for s in x[0:3]])
-    )
-    itl_aggregated_data["top_3_green_skills_names"] = itl_aggregated_data[
-        "top_5_green_skills"
-    ].apply(lambda x: ", ".join([s["skill_name"] for s in x[0:3]]))
-    itl_aggregated_data["top_3_socs_names"] = itl_aggregated_data["top_5_socs"].apply(
-        lambda x: ", ".join([s["soc_name"] for s in x[0:3]])
-    )
-
-    # Get the relative greenness measures across regions
-    itl_aggregated_data["occ_greenness"] = itl_aggregated_data[
-        "average_occ_green_timeshare"
-    ].apply(
-        lambda x: pg.categorical_assign(
-            x, itl_aggregated_data["average_occ_green_timeshare"]
+    for agg_itl_by in ["itl_2_code", "itl_3_code"]:
+        itl_aggregated_data = pg.create_agg_data(
+            all_green_measures_df,
+            all_skills_df,
+            soc_descriptions_dict=None,
+            agg_col=agg_itl_by,
+            job_id_col="job_id",
         )
-    )
-    itl_aggregated_data["ind_greenness"] = itl_aggregated_data[
-        "average_ind_perunit_ghg"
-    ].apply(
-        lambda x: pg.categorical_assign(
-            x, itl_aggregated_data["average_ind_perunit_ghg"], rev=True
+
+        # Clean up for tooltips in plotting
+        itl_aggregated_data["top_3_sics_names"] = itl_aggregated_data[
+            "top_5_sics"
+        ].apply(lambda x: ", ".join([s["sic_name"] for s in x[0:3]]))
+        itl_aggregated_data["top_3_green_skills_names"] = itl_aggregated_data[
+            "top_5_green_skills"
+        ].apply(lambda x: ", ".join([s["skill_name"] for s in x[0:3]]))
+        itl_aggregated_data["top_3_socs_names"] = itl_aggregated_data[
+            "top_5_socs"
+        ].apply(lambda x: ", ".join([s["soc_name"] for s in x[0:3]]))
+
+        # Get the relative greenness measures across regions
+        itl_aggregated_data["occ_greenness"] = itl_aggregated_data[
+            "average_occ_green_timeshare"
+        ].apply(
+            lambda x: pg.categorical_assign(
+                x, itl_aggregated_data["average_occ_green_timeshare"]
+            )
         )
-    )
-
-    itl_aggregated_data["skills_greenness"] = itl_aggregated_data[
-        "average_prop_green_skills"
-    ].apply(
-        lambda x: pg.categorical_assign(
-            x, itl_aggregated_data["average_prop_green_skills"]
+        itl_aggregated_data["ind_greenness"] = itl_aggregated_data[
+            "average_ind_perunit_ghg"
+        ].apply(
+            lambda x: pg.categorical_assign(
+                x, itl_aggregated_data["average_ind_perunit_ghg"], rev=True
+            )
         )
-    )
 
-    itl_aggregated_data["greenness_score"] = itl_aggregated_data.apply(
-        lambda x: pg.get_one_score(
-            x["occ_greenness"], x["ind_greenness"], x["skills_greenness"]
-        ),
-        axis=1,
-    )
+        itl_aggregated_data["skills_greenness"] = itl_aggregated_data[
+            "average_prop_green_skills"
+        ].apply(
+            lambda x: pg.categorical_assign(
+                x, itl_aggregated_data["average_prop_green_skills"]
+            )
+        )
 
-    today = datetime.now().strftime("%Y%m%d")
-    save_to_s3(
-        BUCKET_NAME,
-        itl_aggregated_data,
-        f"outputs/data/ojo_application/extracted_green_measures/analysis/{agg_itl_by}_aggregated_data_{today}.csv",
-    )
+        itl_aggregated_data["greenness_score"] = itl_aggregated_data.apply(
+            lambda x: pg.get_one_score(
+                x["occ_greenness"], x["ind_greenness"], x["skills_greenness"]
+            ),
+            axis=1,
+        )
+
+        today = datetime.now().strftime("%Y%m%d")
+        save_to_s3(
+            BUCKET_NAME,
+            itl_aggregated_data,
+            f"outputs/data/ojo_application/extracted_green_measures/analysis/{agg_itl_by}_aggregated_data_{today}.csv",
+        )
