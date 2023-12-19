c34df3736d03eb7d4d651c481e06f18aee868cb1
India <46863334+india-kerle@users.noreply.github.com>
2023-12-19 15:44:42 +0000

    Violin viz (#117)
    * add violin plots graph
    
    * update yaxis name
    
    * update to use same aggregate dfs
    
    * update to use log; remove dot sizes
    
    * address liz's PR

--- dap_prinz_green_jobs/analysis/ojo_analysis/aggregate_by_region.py
+++ dap_prinz_green_jobs/analysis/ojo_analysis/aggregate_by_region.py
@@ -7,7 +7,7 @@ python dap_prinz_green_jobs/analysis/ojo_analysis/aggregate_by_region.py
 
 import dap_prinz_green_jobs.analysis.ojo_analysis.process_ojo_green_measures as pg
 from dap_prinz_green_jobs.getters.data_getters import save_to_s3
-from dap_prinz_green_jobs import BUCKET_NAME, PROJECT_DIR, analysis_config
+from dap_prinz_green_jobs import BUCKET_NAME, analysis_config
 from dap_prinz_green_jobs.getters.ojo_getters import (
     get_mixed_ojo_location_sample,
     get_mixed_ojo_salaries_sample,
@@ -16,8 +16,6 @@ from dap_prinz_green_jobs.getters.ojo_getters import (
 )
 
 from datetime import datetime
-import yaml
-import os
 
 if __name__ == "__main__":
     job_id_col = analysis_config["job_id_col"]
--- dap_prinz_green_jobs/notebooks/README.md
+++ dap_prinz_green_jobs/notebooks/README.md
@@ -33,3 +33,7 @@ The `2x2_typologies.ipynb` notebook contains code to create a 2x2 typology of gr
 ### New Skills analysis
 
 The `new_skills_analysis.ipynb` notebook contains code to create graphs that explore new 'green' skills for the comparisons flow of the Green Jobs Explorer.
+
+### Violin plots
+
+The `violin_plots.ipynb` notebook contains code to create violin plots of the greenness measures for the the Green Jobs Explorer.
